2b41f415189d2e45832ffaee8557d7437b54687f
Scott Prue <prescottprue@gmail.com>
2015-08-13 23:19:23 -0400

    Mock api handler added to unit tests. Tests passing with 58% coverage.

--- test/unit/matter-client.spec.js
+++ test/unit/matter-client.spec.js
@@ -1,69 +1,103 @@
-import Matter from '../../src/matter-client';
-
+import MatterClient from '../../src/matter-client';
+import axios from 'axios';
+let Matter = new MatterClient();
+let mockGet = sinon.stub(axios, 'get', function() {
+ console.log('mock get called with:', arguments);
+ return new Promise((resolve) => {
+   resolve({data: {}});
+ });
+});
+let mockPut = sinon.stub(axios, 'put', function() {
+ console.log('mock put called with:', arguments);
+ return new Promise((resolve) => {
+   resolve({data: {}});
+ });
+});
+let mockPost = sinon.stub(axios, 'post', function(url, postData) {
+ console.log('mock post called with:', arguments);
+ return new Promise((resolve, reject) => {
+   if (!postData || postData == {}) {
+     reject({});
+   }
+   resolve({data: {}});
+ });
+});
+let mockAxiosInterceptor = sinon.stub(axios.interceptors.request,'use',function(config) {
+ console.log('mock post called with:', arguments);
+});
 describe('Matter', () => {
-  describe('Login method', () => {
-    beforeEach(() => {
-      spy(Matter, 'login');
-      spy(console, 'error');
-    });
+ describe('auth interceptor', () => {
+   it('should have been run once', () => {
+     axios.interceptors.request.use(function(config) {
+       expect(config).to.be.an('object');
+     });
+   });
+ });
+ describe('Login method', () => {
+   beforeEach(() => {
+     spy(Matter, 'login');
+     spy(console, 'error');
+   });
+   it('handles no input', () => {
+     Matter.login();
+     expect(Matter.login).to.have.been.calledOnce;
+   });
+   it('logs in user', () => {
+     //window.sessionStorage.setItem();
+     Matter.login({username: 'test', password: 'test'});
+     expect(Matter.login).to.have.been.calledOnce;
+   });
+ });
+ describe('Signup method', () => {
+   beforeEach(() => {
+     spy(Matter, 'signup');
+   });
+   it('handles no input', () => {
+     Matter.signup();
+     expect(Matter.signup).to.have.been.calledOnce;
+   });
+   it('signs up new user', () => {
+     Matter.signup({username: 'test', password: 'test'});
+     expect(Matter.signup).to.have.been.calledOnce;
+   });
+ });
+ describe('Logout method', () => {
+   beforeEach(() => {
+     spy(Matter, 'logout');
+   });
 
-    it('should have been run once', () => {
-      Matter.login({username: 'test', password: 'test'});
-      expect(Matter.login).to.have.been.calledOnce;
-    });
-    it('handle no input', () => {
-      Matter.login();
-      expect(Matter.login).to.have.been.calledOnce;
-    });
-  });
-  describe('Signup method', () => {
-    beforeEach(() => {
-      spy(Matter, 'signup');
-      Matter.signup({username: 'test', password: 'test'});
-    });
+    it('logs user', () => {
+     Matter.logout();
+     expect(Matter.logout).to.have.been.calledOnce;
+   });
+    //TODO: Check that token is removed from local storage
+ });
+ describe('getCurrentUser method', () => {
+   beforeEach(() => {
+     spy(Matter, 'getCurrentUser');
+     Matter.getCurrentUser();
+   });
 
-    it('should have been run once', () => {
-      expect(Matter.signup).to.have.been.calledOnce;
-    });
+   it('should have been run once', () => {
+     expect(Matter.getCurrentUser).to.have.been.calledOnce;
+   });
+ });
+ describe('getAuthToken method', () => {
+   beforeEach(() => {
+     spy(Matter, 'getAuthToken');
+   });
 
-  });
-  describe('Logout method', () => {
-    beforeEach(() => {
-      spy(Matter, 'logout');
-      Matter.logout();
-    });
-
-    it('should have been run once', () => {
-      expect(Matter.logout).to.have.been.calledOnce;
-    });
-
-  });
-  describe('getCurrentUser method', () => {
-    beforeEach(() => {
-      spy(Matter, 'getCurrentUser');
-      Matter.getCurrentUser();
-    });
-
-    it('should have been run once', () => {
-      expect(Matter.getCurrentUser).to.have.been.calledOnce;
-    });
-  });
-  describe('getAuthToken method', () => {
-    beforeEach(() => {
-      spy(Matter, 'getAuthToken');
-    });
-
-    it('should have been run once', () => {
-      Matter.getAuthToken();
-      expect(Matter.getAuthToken).to.have.been.calledOnce;
-    });
-    it('get auth token', () => {
-      window = {localStorage: {}};
-      window.localStorage.getItem = () => {
-        return '';
-      };
-      Matter.getAuthToken();
-      expect(Matter.getAuthToken).to.have.been.calledOnce;
-    });
-  });
+   it('should have been run once', () => {
+     Matter.getAuthToken();
+     expect(Matter.getAuthToken).to.have.been.calledOnce;
+   });
+   it('get auth token', () => {
+     window = {sessionStorage: {}};
+     window.sessionStorage.getItem = () => {
+       return '';
+     };
+     Matter.getAuthToken();
+     expect(Matter.getAuthToken).to.have.been.calledOnce;
+   });
+ });
 });
