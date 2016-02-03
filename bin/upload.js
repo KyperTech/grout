3f65c0a70594a56e8758263c40c1637f25b2a593
Scott Prue <prescottprue@gmail.com>
2016-02-03 00:20:00 -0500

    Version v0.2.2
    * File upload/download working
    * Add/Remove collaborator
    * Matter version v0.2.3
    * Firebase and other deps updated

--- bin/upload.js
+++ bin/upload.js
@@ -1,16 +1,16 @@
 (function(){
-  const conf = require('../config.json');
+  const config = require('../config.json');
   const pkg = require('../package.json');
   const exec = require('child_process').exec;
-  const versionUrl = 'kyper-cdn/js/grout/' + pkg.version + '/';
-  const latestUrl = 'kyper-cdn/js/grout/latest/';
+  const versionUrl = config.cdn.bucket + '/' + config.cdn.prefix + '/' + pkg.version + '/';
+  const latestUrl = config.cdn.bucket + '/' + config.cdn.prefix + '/latest/';
   //Upload to version url then to latest url
   uploadToUrl(versionUrl, () => {
     uploadToUrl(latestUrl);
   });
   function uploadToUrl(url, cb){
     console.log('Uploading to:', url);
-    var child = exec(`s3-cli --config ~/.s3cfg sync dist s3://${url}`, (error) => {
+    const child = exec(`s3-cli --config ~/.s3cfg sync dist s3://${url}`, (error) => {
       if (error !== null) {
         console.log('error uploading to S3 url: ' + url);
         console.log(error.toString() || error);
